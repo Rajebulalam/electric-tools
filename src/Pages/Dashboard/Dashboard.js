cd158294ca5fda8c615557a50891814542a7ace9
unknown <rajebul.alam17@gmail.com>
2022-05-29 16:37:36 +0600

    Dashboard Side Bar Edited

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -5,12 +5,12 @@ const Dashboard = () => {
     return (
         <section className='px-4 sm:px-10 py-10'>
             <div className="drawer drawer-mobile">
-                <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col p-6 ml-4 rounded-md bg-slate-200">
                     <Outlet></Outlet>
                 </div>
                 <div className="drawer-side bg-slate-200 rounded">
-                    <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
+                    <label for="my-drawer-2" className="drawer-overlay"></label>
                     <ul className="menu p-4 overflow-y-auto w-80 text-base-content bg-slate-200 lg:bg-transparent rounded-md">
                         <li className='font-medium hover:text-secondary'><Link to='/dashboard'>My Orders</Link></li>
                         <li className='font-medium hover:text-secondary'><Link to='/dashboard/reviews'>Add Reviews</Link></li>
--- src/Pages/HomePage/Newsletter.js
+++ src/Pages/HomePage/Newsletter.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Newsletter = () => {
     return (
-        <section className='px-4 py-10 sm:p-14 bg-white rounded'>
+        <section className='px-0 py-10 sm:p-14 bg-white rounded'>
             <h2 style={{ fontFamily: 'Merienda' }} className='text-center font-bold text-3xl text-secondary'>Get The Latest Deals</h2>
             <p className='text-center text-gray-600 font-medium pt-4'>To get our latest news and offers connect with us.</p>
-            <div className='flex items-center justify-center pt-6'>
+            <div className='flex items-center justify-center px-3 pt-6'>
                 <div className="form-control">
                     <div className="input-group">
                         <input className='px-4 border' type="email" name="email" id="email" placeholder='Your Email' />
